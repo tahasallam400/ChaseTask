1769f393f71ab808aa4742c199b2162ec42b544f
tahasallam400 <taha.abderahman.metwally@gmail.com>
2024-08-31 12:37:52 -0500

    ..

---
+++ ChaseTask/ChaseTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseTaskApp.swift
+//  ChaseTask
+//
+//  Created by Taha Metwally on 30/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChaseTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaseTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChaseTask
+//
+//  Created by Taha Metwally on 30/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
